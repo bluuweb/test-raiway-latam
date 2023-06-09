071f1947156872c18e47b9790af9a434f1c2ae6b
bluuweb <contacto@bluuweb.cl>
2023-06-08 21:32:55 -0400

    test 2

--- database/connection.js
+++ database/connection.js
@@ -4,7 +4,7 @@ dotenv.config();
 import pkg from "pg";
 const { Pool } = pkg;
 
-// const connectionString = process.env.CONNECT_RAIWAY;
+const connectionString = process.env.CONNECT_RAIWAY;
 
-export const pool = new Pool({ allowExitOnIdle: true });
+export const pool = new Pool({ connectionString, allowExitOnIdle: true });
 export default pool;
