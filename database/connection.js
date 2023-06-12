8161e689c80edc6c606c2b04d6af2420b6a4a6e1
bluuweb <contacto@bluuweb.cl>
2023-06-12 09:22:07 -0400

    connectionString

--- database/connection.js
+++ database/connection.js
@@ -6,5 +6,8 @@ const { Pool } = pkg;
 
 const connectionString = process.env.CONNECT_RAIWAY;
 
-export const pool = new Pool({ connectionString, allowExitOnIdle: true });
+export const pool = connectionString
+  ? new Pool({ connectionString, allowExitOnIdle: true })
+  : new Pool({ allowExitOnIdle: true });
+
 export default pool;
